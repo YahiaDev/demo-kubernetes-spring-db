7f8d07be4886c318a7e1a0cfcdc6464da3acc565
yahiaDev <yahia.ammar.info@gmail.com>
2021-04-15 14:24:47 +0200

    add logs

--- src/main/java/com/demokubernetesspringdb/test/TestController.java
+++ src/main/java/com/demokubernetesspringdb/test/TestController.java
@@ -1,6 +1,7 @@
 package com.demokubernetesspringdb.test;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/test")
 @AllArgsConstructor
+@Slf4j
 public class TestController {
 
 	private final TestService testService;
 
 	@GetMapping("/findAll")
 	public ResponseEntity<List<Test>> findAll() {
+		log.info("find all method v0.17");
 		return ResponseEntity.ok(this.testService.findAll());
 	}
 }
